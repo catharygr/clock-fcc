5d611b02a8c7577d9f9c542e4d3e50b710f791fa
catharygr <103951447+catharygr@users.noreply.github.com>
2023-05-22 12:54:29 +0100

    Solucionada la lógica ahora falta el audio

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,31 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import "../style.css";
 
 function App() {
   const [breakLength, setBreakLength] = useState(5);
   const [sessionLength, setSessionLength] = useState(25);
-  const [timeLeft, seiTmeLeft] = useState("25:00");
+  const [timeLeft, setTimeLeft] = useState("25:00");
+  const [isStartClock, setIsStartClock] = useState(true);
+  const [timerLabel, setTimerLaber] = useState("Session");
+  const [isBreak, setIsBreak] = useState(false);
 
   function handleReset() {
     setBreakLength(5);
     setSessionLength(25);
-    seiTmeLeft("25:00");
+    setTimeLeft("25:00");
+    sessionLengthSec.current = sessionLength * 60;
+    setTimerLaber("Session");
+    setIsBreak(false);
+    clearInterval(interval.current);
+    setIsStartClock(true);
   }
 
   function handleClockLentgth(e) {
     const id = e.target.id;
+    let sesLong = sessionLength;
+    const sesLongDecrem = sesLong <= 1 ? sesLong : sesLong - 1;
+    const sesLongIncrem = sesLong >= 60 ? sesLong : sesLong + 1;
+
     switch (id) {
       case "break-decrement":
         setBreakLength((oldValue) =>
@@ -26,16 +38,52 @@ function App() {
         );
         break;
       case "session-decrement":
-        setSessionLength((oldValue) =>
-          oldValue <= 1 ? oldValue : (oldValue -= 1)
+        setSessionLength(sesLongDecrem);
+        setTimeLeft(
+          sesLongDecrem < 10 ? `0${sesLongDecrem}:00` : `${sesLongDecrem}:00`
         );
         break;
       case "session-increment":
-        setSessionLength((oldValue) =>
-          oldValue >= 60 ? oldValue : (oldValue += 1)
+        setSessionLength(sesLongIncrem);
+        setTimeLeft(
+          sesLongIncrem < 10 ? `0${sesLongIncrem}` : `${sesLongIncrem}:00`
         );
         break;
     }
+  }
+
+  const interval = useRef(null);
+  const sessionLengthSec = useRef(sessionLength * 60);
+  function startClock() {
+    sessionLengthSec.current -= 1;
+    let isBreakTrue = isBreak;
+    interval.current = setInterval(() => {
+      let min = Math.floor(sessionLengthSec.current / 60);
+      let sec = sessionLengthSec.current % 60;
+      if (min < 10) min = `0${min}`;
+      if (sec < 10) sec = `0${sec}`;
+      setTimeLeft(min + ":" + sec);
+      sessionLengthSec.current -= 1;
+      if (sessionLengthSec.current < 0 && !isBreakTrue) {
+        setTimerLaber("Break");
+        setIsBreak(true);
+        sessionLengthSec.current = breakLength * 60;
+        isBreakTrue = true;
+      }
+      if (sessionLengthSec.current < 0 && isBreakTrue) {
+        setTimerLaber("Session");
+        setIsBreak(false);
+        sessionLengthSec.current = sessionLength * 60;
+        isBreakTrue = false;
+      }
+    }, 1000);
+    setIsStartClock(false);
+  }
+
+  function stopClock() {
+    sessionLengthSec.current -= 1;
+    clearInterval(interval.current);
+    setIsStartClock(true);
   }
 
   return (
@@ -69,10 +117,21 @@ function App() {
         </div>
       </div>
       <div className="display">
-        <h2 id="timer-label">Session</h2>
+        <h2 id="timer-label">{timerLabel}</h2>
         <p id="time-left">{timeLeft}</p>
         <div className="display-control">
-          <button id="start_stop">Start</button>
+          {isStartClock ? (
+            <button onClick={startClock} id="start_stop">
+              {" "}
+              Start
+            </button>
+          ) : (
+            <button onClick={stopClock} id="start_stop">
+              {" "}
+              Stop
+            </button>
+          )}
+
           <button onClick={handleReset} id="reset">
             Reset
           </button>
